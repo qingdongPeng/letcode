b0aa37b7402cc103bbf1db54310a324019c60b0c
qingdongPeng <38778076+qingdongPeng@users.noreply.github.com>
2019-05-13 23:49:24 +0800

    add plusOne and lengthOfLastWord

--- src/main/java/simple/Simple1.java
+++ src/main/java/simple/Simple1.java
@@ -1,6 +1,9 @@
 package simple;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.Arrays;
 
 public class Simple1 {
     @Test
@@ -93,5 +96,76 @@ public class Simple1 {
         int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(maxSubArray(arr));
     }
+
+    /**
+     * 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+     * 如果不存在最后一个单词，请返回 0 。
+     * 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+     * 示例:
+     * 输入: "Hello World"
+     * 输出: 5
+     */
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        String[] str = s.split(" ");
+        if (str.length == 0)
+            return 0;
+        if (str.length == 1)
+            return str[0].length();
+        return str[str.length - 1].length();
+    }
+
+    @Test
+    public void testLengthOfLastWord() {
+        String s = "a ";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    /**
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * 示例 1:
+     * 输入: [1,2,3]
+     * 输出: [1,2,4]
+     * 解释: 输入数组表示数字 123。
+     * 示例 2:
+     * 输入: [4,3,2,1]
+     * 输出: [4,3,2,2]
+     * 解释: 输入数组表示数字 4321。
+     */
+    public int[] plusOne(int[] digits) {
+        int index = digits.length - 1;
+        while (index > 0) {
+            if (digits[index] == 9) {
+                digits[index] = 0;
+                index--;
+            } else {
+                digits[index] += 1;
+                break;
+            }
+        }
+        if (index == -1 && digits[0] == 0) {
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            System.arraycopy(digits, 0, res, 1, digits.length);
+            return res;
+        }
+        return digits;
+    }
+
+    @Test
+    public void testPlusOne() {
+        int[] arr = {4, 3, 2, 1};
+        System.out.println(Arrays.toString(plusOne(arr)));
+
+        int[] arr2 = {8, 8, 9};
+        System.out.println(Arrays.toString(plusOne(arr2)));
+
+        int[] arr3 = {9,9,9};
+        System.out.println(Arrays.toString(plusOne(arr3)));
+    }
+
 }
 
