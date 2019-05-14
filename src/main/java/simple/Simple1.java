169078786dac6c6808116c4b5e8f9d5bccbea65a
qingdongPeng <38778076+qingdongPeng@users.noreply.github.com>
2019-05-14 22:09:50 +0800

    add sqrt impl

--- src/main/java/simple/Simple1.java
+++ src/main/java/simple/Simple1.java
@@ -1,5 +1,6 @@
 package simple;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
 
@@ -167,5 +168,35 @@ public class Simple1 {
         System.out.println(Arrays.toString(plusOne(arr3)));
     }
 
+    /**
+      * 实现 int sqrt(int x) 函数。
+        计算并返回 x 的平方根，其中 x 是非负整数。
+        由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+        示例 1:
+            输入: 4
+            输出: 2
+        示例 2:
+            输入: 8
+            输出: 2
+            说明: 8 的平方根是 2.82842...,
+            由于返回类型是整数，小数部分将被舍去。
+     */
+    public int mySqrt(int x) {
+        //牛顿迭代法
+        if (x == 0)
+            return 0;
+        double cur = x;
+        double last = 0;
+        while(Math.abs(cur - last) >= 1) {
+            last = cur;
+            cur = (cur * cur + x) / (2 * cur);
+        }
+        return (int)Math.floor(cur);
+    }
+
+    @Test
+    public void  testMySqrt() {
+        System.out.println(mySqrt(34));
+    }
 }
 
