ed8628db153bffd92fda8e9327d982ab37b62647
qingdongPeng <38778076+qingdongPeng@users.noreply.github.com>
2019-07-24 23:26:59 +0800

    impl simple addBinary

--- src/main/java/simple/Simple2.java
+++ src/main/java/simple/Simple2.java
@@ -152,4 +152,44 @@ public class Simple2 {
         System.out.println(singleNumberByMath(arr));
         System.out.println(singleNumberByMap(arr));
     }
+
+    /**
+     * 给定两个二进制字符串，返回他们的和（用二进制表示）。
+     * 输入为非空字符串且只包含数字 1 和 0。
+     *
+     * 示例 1:
+     * 输入: a = "11", b = "1"
+     * 输出: "100"
+     *
+     * 示例 2:
+     * 输入: a = "1010", b = "1011"
+     * 输出: "10101"
+     *
+     * 借鉴他人思路
+     */
+    public String addBinary(String a, String b) {
+        StringBuffer sb = new StringBuffer();
+        int carry = 0;
+        int i = a.length() -1;
+        int j = b.length() -1;
+        while(i >= 0 || j >= 0 || carry != 0){
+            if(i >= 0) {
+                carry += a.charAt(i--) - '0';
+            }
+            if(j >= 0) {
+                carry += b.charAt(j--) - '0';
+            }
+            sb.append(carry%2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    @Test
+    public void testAddBinary() {
+        String a = "1010";
+        String b = "1011";
+        System.out.println(addBinary(a, b));
+    }
+
 }
